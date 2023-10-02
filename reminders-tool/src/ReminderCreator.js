2346a50519143a11544c221c4e6d17d07fe190cf
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-02 03:33:40 +0300

    Updates ReminderCreator.js

--- reminders-tool/src/ReminderCreator.js
+++ reminders-tool/src/ReminderCreator.js
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addReminder } from "./actions";
 
-export default function ReminderCreator() {
+function ReminderCreator(props) {
   const [message, setMessage] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-    setMessage(message);
+    props.addReminder(message);
+    setMessage("");
   }
 
   function handleChange(event) {
     setMessage(event.target.value);
   }
-return (
-  <div className="ReminderCreator">
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Type Reminder here..."
-        autoFocus="on"
-        className="form-control w-100 text-center"
-        onChange={handleChange}
-      />
-      <input
-        type="submit"
-        value="Add Reminder"
-        className="btn btn-primary w-100"
-      />
-    </form>
-  </div>
-);
+
+  return (
+    <div className="ReminderCreator">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type Reminder here..."
+          autoFocus="on"
+          className="form-control w-100 text-center"
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="Add Reminder"
+          className="btn btn-primary w-100"
+        />
+      </form>
+    </div>
+  );
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addReminder: (text) => dispatch(addReminder(text)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ReminderCreator);
