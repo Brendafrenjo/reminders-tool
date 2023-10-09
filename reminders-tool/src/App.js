dddae9c01941e6facb4327f221b5da970cf04565
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-09 23:43:29 +0300

    Update App.js

--- reminders-tool/src/App.js
+++ reminders-tool/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { connect } from "react-redux";
 import ReminderCreator from "./ReminderCreator";
+import ReminderContainer from "./ReminderContainer";
 
 function App() {
   return (
     <div className="App">
       <div>Add Reminders</div>
       <ReminderCreator />
+      <ReminderContainer />
     </div>
   );
 }
